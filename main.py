b578a80c6d64b281eee61336e7a94b5644b2fa3f
shivamkr0037 <130444141+shivamkr0037@users.noreply.github.com>
2023-05-19 23:26:25 +0530

    Updated

---
+++ main.py
@@ -0,0 +1,48 @@
+import logging
+import os
+import json
+import random
+import sqlite3
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+
+from utils.handlers import (
+    start,
+    show_categories,
+    select_category,
+    answer,
+    score,
+    highscores,
+    leaderboard,
+    end,
+    next_question,
+)
+from utils.database import setup_database, get_score, update_highscore, get_highscores, get_global_highscores
+from utils.models import QuizBot, load_categories, parse_questions
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+def main():
+    with open('token.txt', 'r') as f:
+        token = f.read().strip()
+
+    # Set up the SQLite database for high scores
+    setup_database()
+
+    # Load the questions from the categories folder
+    categories = load_categories()
+
+    # Initialize the QuizBot
+    quiz_bot = QuizBot(token, categories)
+
+    # Register the handlers
+    quiz_bot.register_handlers()
+
+    # Run the bot
+    quiz_bot.run()
+
+
+if __name__ == '__main__':
+    main()
